f7a8328b5bdbcb9447feb0e832a0d3ae86e7a06e
AlexanderDV <AlexanderDV.ru@gmail.com>
2020-07-08 10:38:27 +0500

    20.07.08 10:36 Add weak indexing, correct adding, add to rudazov, toaru, badchoice, my

--- Assets/NewPageGen.cs
+++ Assets/NewPageGen.cs
@@ -1,71 +1,85 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using System.IO;
 
 public enum Type
 {
-    Page,
-    Feature,
-    Ability,
-    Verse,
-    Person,
-    Creature
+    ability,
+	complex,
+	skill,
+	element,
+	emotion,
+	force,
+	form,
+	matter,
+	substance,
+	creature,
+	dimension,
+	feature,
+	inheritance,
+	knowledge,
+	location,
+	_object__reads_like_just_object,
+	parent,
+	person,
+	reference,
+	society,
+	text,
+	type,
+	verse
 }
 [ExecuteInEditMode]
 public class NewPageGen : MonoBehaviour
 {
-    public string path = "Pages/";
-    public Type parent=Type.Page;
+    public string assetsPath="Assets/",pagesPath="pages/",pagePath = "",pageExtension=".page";
+	public string pageName="new_page",pageVerse="new_verse";
+    public Type pageParent=Type.verse;
     public string custom = "";
     public bool create;
+	public bool index;
+	public InputField field;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+		string path=assetsPath+pagesPath+pagePath+pageName+pageExtension;
+		if(index)
+		{
+			string indexed="";
+			var subdirs=new DirectoryInfo(assetsPath+pagesPath).GetDirectories();
+			var files=new DirectoryInfo(assetsPath+pagesPath).GetFiles();
+			foreach(var file in files)
+				if(file.Name.EndsWith(pageExtension))
+					indexed+=file.Name.Substring(0,file.Name.Length-pageExtension.Length)+" : "+("undefined")+"\n";
+
+			field.text=indexed;
+			index=false;
+		}
         if(create)
         {
-            string p = "Assets";
+            string p = assetsPath;
             foreach (string pp in path.Split('/'))
             {
-                p += "/" + pp;
-                if(p!= "Assets"+"/"+path)
+                p += p==assetsPath?pp:"/" + pp;
+                if(p!= assetsPath+path)
                     if(!new DirectoryInfo(p).Exists)
                         new DirectoryInfo(p).Create();
             }
-            StreamWriter writer = new StreamWriter("Assets/"+path+".page", false);
+            StreamWriter writer = new StreamWriter(path, false);
             string name = path.Split('/')[path.Split('/').Length - 1];
-            string par = "";
-            switch(parent)
-            {
-                case Type.Page:
-                    par = "Page";
-                    break;
-                case Type.Feature:
-                    par = "Feature";
-                    break;
-                case Type.Ability:
-                    par = "Ability";
-                    break;
-                case Type.Verse:
-                    par = "Verse";
-                    break;
-                case Type.Person:
-                    par = "Person";
-                    break;
-                case Type.Creature:
-                    par = "Creature";
-                    break;
-            }
+            string parent = pageParent.ToString().Replace("_object__reads_like_just_object","object");
             if (custom != "")
-                par = custom;
-            writer.WriteLine("name=" + name);
-            writer.WriteLine("parent=" +par);
+                parent = custom;
+            writer.WriteLine("name=\"" + pageName.Replace("_"," ")+"\"");
+            writer.WriteLine("parent=" +parent);
+            writer.WriteLine("verse=" +pageVerse);
             writer.Close();
 
             create = false;
